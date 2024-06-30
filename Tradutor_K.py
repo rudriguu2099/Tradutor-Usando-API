a92fca185ae358adb179605feb163d6527d00ea3
rudriguu2099 <eusourodrigo895@gmail.com>
2024-06-29 23:45:36 -0300

    Atualização 1.1: Agora o usuáro pode escolher o idioma p/ traduzir

--- Tradutor_K.py
+++ Tradutor_K.py
@@ -1,9 +1,31 @@
 from deep_translator import GoogleTranslator
 
-tradutor = GoogleTranslator(source="auto", target="en")
+def traduzir_txt():
+    Linguagens_suportadas = {
+        "english": "en",
+        "portuguese": "pt",
+        "spanish": "es",
+        "french": "fr",
+        "german": "de",
+        "italian": "it",
+        "japanese": "ja",
+        "chinese": "zh",
+        "russian": "ru"
+    }
+    
+    text = input("Digite o texto que você deseja traduzir: ")
 
-texto = input("digite aqui o que vc quer traduzir: ")
+    print("Idiomas disponíveis:")
+    for ling in Linguagens_suportadas:
+        print(f"- {ling.capitalize()}")
 
-traducao = tradutor.translate(texto)
+    linguagem_target = input("Digite o idioma de destino (em inglês): ").strip().lower()
 
-print(f"Sua frase em traduzida é \"{traducao}\"")
+    if linguagem_target in Linguagens_suportadas:
+        codigo_target = Linguagens_suportadas[linguagem_target]
+        txt_traduzido = GoogleTranslator(target=codigo_target).translate(text)
+        print(f"Tradução para {linguagem_target.capitalize()}: {txt_traduzido}")
+    else:
+        print("Desculpe, o idioma não é suportado ou não foi reconhecido.")
+
+traduzir_txt()
