4f105d3c533e57752ebce7e3243b724e349b05ee
rudriguu2099 <eusourodrigo895@gmail.com>
2024-07-01 09:22:45 -0300

    Atualização 1.2: Organização do código

--- Tradutor_K.py
+++ Tradutor_K.py
@@ -2,30 +2,35 @@ from deep_translator import GoogleTranslator
 
 def traduzir_txt():
     Linguagens_suportadas = {
-        "english": "en",
-        "portuguese": "pt",
-        "spanish": "es",
-        "french": "fr",
-        "german": "de",
-        "italian": "it",
-        "japanese": "ja",
-        "chinese": "zh",
-        "russian": "ru"
+        "inglês": "en",
+        "português": "pt",
+        "espanhol": "es",
+        "francês": "fr",
+        "alemão": "de",
+        "italiano": "it",
+        "japonês": "ja",
+        "chinês": "zh",
+        "russo": "ru"
     }
-    
-    text = input("Digite o texto que você deseja traduzir: ")
-
-    print("Idiomas disponíveis:")
-    for ling in Linguagens_suportadas:
-        print(f"- {ling.capitalize()}")
-
-    linguagem_target = input("Digite o idioma de destino (em inglês): ").strip().lower()
-
-    if linguagem_target in Linguagens_suportadas:
-        codigo_target = Linguagens_suportadas[linguagem_target]
-        txt_traduzido = GoogleTranslator(target=codigo_target).translate(text)
-        print(f"Tradução para {linguagem_target.capitalize()}: {txt_traduzido}")
-    else:
-        print("Desculpe, o idioma não é suportado ou não foi reconhecido.")
+    print("[1] Traduzir\n[2] Lista de Idiomas\n[3] Sair")
+    while True:
+        comando = input("\nDigite o comando: ").strip()
+        if comando == '1':
+            text = input("Digite o texto que você deseja traduzir: ")
+            linguagem_target = input("Digite o idioma de destino: ").strip().lower()
+            if linguagem_target in Linguagens_suportadas:
+                codigo_target = Linguagens_suportadas[linguagem_target]
+                txt_traduzido = GoogleTranslator(target=codigo_target).translate(text)
+                print(f"Tradução para {linguagem_target.capitalize()}: {txt_traduzido}")
+            else:
+                print("Desculpe, o idioma não é suportado ou não foi reconhecido.")
+        if comando == '2':
+            print()
+            print("Idiomas disponíveis:")
+            for ling in Linguagens_suportadas:
+                print(f"- {ling.capitalize()}")
+        if comando == '3':
+            print("Encerrando o programa...")
+            break
 
 traduzir_txt()
